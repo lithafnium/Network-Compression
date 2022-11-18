22df1989943452cf6c4599c6da9b743a51ff4d72
Leonard Tang <leonardgentwintang@gmail.com>
2022-11-17 20:19:00 -0800

    Begin decoupling training code loop into single evaluations

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.utils.data import (
 
 from collections import defaultdict
 
-from graph_model import GraphModel
+from graph_model import BlockModel, UnSqueeze
 from size_estimator import SizeEstimator
 import tqdm
 import numpy as np
@@ -52,6 +52,7 @@ class Trainer:
     ):
         self.criterion = nn.CrossEntropyLoss()
         self.graph_sizes = [100, 1000]
+        # self.graph_sizes = [1000]
         self.graph_densities = [0.25, 0.5, 0.75]
         # self.graph_densities = [0.5, 0.75]
         self.min_layers = min_layers
@@ -66,41 +67,53 @@ class Trainer:
         plt.plot([i + 1 for i in range(num_epochs)], loss_values)
         plt.savefig(f"plots/{path}.png")
 
-    def get_data(self, path):
+    def get_data(self, path, oversample=False):
+        """ 
+        Read and load a mtx file as EdgeDataset 
+        """
         a = mmread(path)
         labels = []
         edges = []
 
-        add_edges = []
+        add_ones, add_zeros = [], []
         ones = 0
         zeros = 0
         for i in range(len(a)):
             for j in range(len(a)):
                 edges.append([i, j])
-                # TODO(Leonard): clean up this oversampling code
+                # TODO(Leonard): clean up this oversampling code; i.e. if oversample: then
                 if a[i][j] == 1:
                     labels.append(1)
-                    # add_edges.append([i,j])
-                    # ones += 1
+                    add_ones.append([i,j])
+                    ones += 1
                 else:
                     labels.append(0)
-                    # zeros += 1
+                    add_zeros.append([i,j])
+                    zeros += 1
         # TODO(Leonard): same here, clean up this oversampling code
-        # og_labels = np.array(copy.deepcopy(labels))
-        # og_edges = np.array(copy.deepcopy(edges))
-        # while ones < zeros:
-        #     edges.append(random.choice(add_edges))
-        #     labels.append(1)
-        #     ones += 1
+        og_labels = np.array(copy.deepcopy(labels))
+        og_edges = np.array(copy.deepcopy(edges))
+        if ones < zeros:
+            while ones < zeros:
+                edges.append(random.choice(add_ones))
+                labels.append(1)
+                ones += 1
+        else:
+            while ones < zeros:
+                edges.append(random.choice(add_zeros))
+                labels.append(0)
+                zeros += 1
         labels = np.array(labels)
         edges = np.array(edges)
+        print("labels one???", sum(labels))
+        print("labels total???", len(labels))
 
         train_dataset = EdgeDataset(edges, labels)
-        # val_dataset = EdgeDataset(og_edges, og_labels)
-        val_dataset = EdgeDataset(edges, labels)
+        val_dataset = EdgeDataset(og_edges, og_labels)
+        # val_dataset = EdgeDataset(edges, labels)
         return train_dataset, val_dataset
 
-    def eval(self, model: GraphModel, val_dataloader: DataLoader, path: str):
+    def eval(self, model, val_dataloader: DataLoader, path: str):
         print("Evaluating...")
         model.eval()
         val_acc = 0
@@ -135,7 +148,7 @@ class Trainer:
 
     def train(
         self,
-        model: GraphModel,
+        model,
         train_dataloader,
         val_dataloader,
         epochs,
@@ -153,18 +166,22 @@ class Trainer:
 
             train_acc = 0
             for x_train_batch, y_train_batch in train_dataloader:
+                # print("x_train_batch size", x_train_batch.size())
                 b_nodes = x_train_batch.to(device)
                 b_labels = y_train_batch.to(device)
 
                 optimizer.zero_grad()
 
                 y_train_pred = model(b_nodes)
-                y_pred_softmax = torch.log_softmax(y_train_pred, dim=1)
-                _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
+                # y_pred_softmax = torch.log_softmax(y_train_pred, dim=1)
+                _, y_pred_tags = torch.max(y_train_pred, dim=1)
 
+                # print("b_labels", b_labels)
                 # print("y_pred_tags", y_pred_tags)
+                # print("y_train_pred", y_train_pred)
 
                 train_loss = self.criterion(y_train_pred, b_labels)
+                # print("train_loss", train_loss)
                 train_loss.backward()
                 optimizer.step()
 
@@ -186,50 +203,78 @@ class Trainer:
 
         # torch.save(model.state_dict(), f"./models/{path}.pt")
 
-    def train_and_eval(self, batch_size=16, epochs=1000):
+    def train_and_eval_single_graph_with_model(self, model, data_path=None, batch_size=16, epochs=1000):
+        train_dataset, val_dataset = self.get_data(data_path)
+        train_dataloader = DataLoader(
+            train_dataset,
+            sampler=RandomSampler(train_dataset),  # Sampling for training is random
+            batch_size=batch_size,
+        )
+
+        evaluation_dataloader = DataLoader(
+            val_dataset,
+            sampler=SequentialSampler(
+                val_dataset
+            ),  # Sampling for validation is sequential as the order doesn't matter.
+            batch_size=batch_size,
+        )
+
+        self.train(
+            model,
+            train_dataloader,
+            evaluation_dataloader,
+            epochs=epochs,
+            # path=f"squeeze-model-{num_layers}-{num_nodes}-{graph_size}-{graph_density}",
+            # Should also record type of data we ran on, e.g. {Erdos-Renyi} or {Small-World}, 
+            # and whether or not we oversampled or not: e.g. {Oversampled} or {}
+            # Ordering: model specs -- 
+            # Should also save training specs 
+            # All this would be solved if we used WANDB
+            path="unsqueeze-regularsample-ErdosRenyi-100-0.25"
+        )
+        
+
+    # TODO(leonard): def this out to run single experiments instead of things in loops
+    # Def out graph and model layers; specify model first, then data
+    def train_and_eval_all_graphs_and_models(self, batch_size=16, epochs=1000):
+        """
+        Run experiments across all types of graph data and models
+        """
+
         for graph_size in self.graph_sizes:
             for graph_density in self.graph_densities:
-                # print(f"Grabbing graph-{graph_size}-{graph_density}.mtx")
-                # train_dataset, val_dataset = self.get_data(f"data/graph-{graph_size}-{graph_density}.mtx")
-                print(f"Grabbing graph-{graph_size}-{graph_density}-small-world.mtx")
-                train_dataset, val_dataset = self.get_data(f"data/graph-{graph_size}-{graph_density}-small-world.mtx")
+                data_path = f"data/graph-{graph_size}-{graph_density}-Erdos-Renyi.mtx"
+                # data_path = f"data/graph-{graph_size}-{graph_density}-small-world.mtx"
+                print(f"Grabbing {data_path}")
+                
+                model = UnSqueeze(2, 2)
+                model.to(device)
+                # model = torch.nn.DataParallel(model)
+                self.train_and_eval_single_graph_with_model(model, data_path, batch_size, epochs)
 
-                train_dataloader = DataLoader(
-                    train_dataset,
-                    sampler=RandomSampler(train_dataset),  # Sampling for training is random
-                    batch_size=batch_size,
-                    # multiprocessing_context='spawn',
-                    # pin_memory=True,
-                    # num_workers=2,
-                )
+                # for num_layers in range(self.min_layers, self.max_layers + 1):
+                #     num_nodes = self.min_nodes
+                #     while num_nodes <= self.max_nodes:
+                #         model = GraphModel(
+                #             2, 2, num_layers=num_layers, num_nodes=num_nodes
+                #         )
+                #         model.to(device)
+                #         # model = torch.nn.DataParallel(model)
+                #         self.train(
+                #             model,
+                #             train_dataloader,
+                #             evaluation_dataloader,
+                #             epochs=epochs,
+                #             # path=f"squeeze-model-{num_layers}-{num_nodes}-{graph_size}-{graph_density}",
+                #             # Should also record type of data we ran on, e.g. {Erdos-Renyi} or {Small-World}, 
+                #             # and whether or not we oversampled or not: e.g. {Oversampled} or {}
+                #             # Ordering: model specs -- 
+                #             # Should also save training specs 
+                #             path="unsqueeze-regularsample-ErdosRenyi-100-0.25"
+                #         )
+                #         num_nodes *= 2
 
-                evaluation_dataloader = DataLoader(
-                    val_dataset,
-                    sampler=SequentialSampler(
-                        val_dataset
-                    ),  # Sampling for validation is sequential as the order doesn't matter.
-                    batch_size=batch_size,
-                    # multiprocessing_context='spawn',
-                    # pin_memory=True,
-                    # num_workers=2,
-                )
-
-                for num_layers in range(self.min_layers, self.max_layers + 1):
-                    num_nodes = self.min_nodes
-                    while num_nodes <= self.max_nodes:
-                        model = GraphModel(
-                            2, 2, num_layers=num_layers, num_nodes=num_nodes
-                        )
-                        model.to(device)
-                        # model = torch.nn.DataParallel(model)
-                        self.train(
-                            model,
-                            train_dataloader,
-                            evaluation_dataloader,
-                            epochs=epochs,
-                            path=f"squeeze-model-{num_layers}-{num_nodes}-{graph_size}-{graph_density}",
-                        )
-                        num_nodes *= 2
+                
 
         print("Saving output loss and size estimates...")
 
